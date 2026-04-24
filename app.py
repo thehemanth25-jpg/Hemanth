11f5f171b2dc71fdba0bb3fee5fb76d181cc5553
thehemanth25-jpg <thehemanth.25@gmail.com>
2026-04-24 09:04:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.linear_model import LogisticRegression
